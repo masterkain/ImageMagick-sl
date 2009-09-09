8ab8295b8c6bf3b79823da53458b8a596e9b0c46
Claudio Poli <kain@kain-osx.local>
2009-09-09 07:38:40 +0200

    less versions to edit if something changes.

--- install_im.sh
+++ install_im.sh
@@ -11,22 +11,25 @@ CONFIGURE_PREFIX="/usr/local" # no trailing slash.
 # Function that tries to download a file, if not abort the process.
 function try_download () {
   file_name=`echo "$1" | ruby -ruri -e 'puts File.basename(gets.to_s.chomp)'` # I cheated.
-  rm -f $file_name
+  rm -f $file_name # Cleanup in case of retry.
   echo "Downloading $1"
   curl --fail --silent -O --url $1
-  result=$?
+  result=$? # Store the code of the last action, should be 0 for a successfull download.
   file_size=`ls -l "$file_name" | awk '{print $5}'`
   # We check for normal http errors, otherwise check the file size
   # since some websites, like sourceforge, redirects and curl can't
   # detect the problem.
   if [[ $result -ne 0 || $file_size -lt 500000 ]] # less than 500K
   then
-    echo "Failed download: $1, size: "$file_size"B, aborting." >&2
+    echo "Failed download: $1, size: "$file_size"B, aborting." >&2 # output on stderr.
     exit 65
+  else
+    echo "Decompressing $file_name"
+    tar zxf $file_name
   fi
 }
 
-# Before running anything try to download all requires files.
+# Before running anything try to download all requires files, saving time.
 try_download http://"$SF_MIRROR".dl.sourceforge.net/project/freetype/freetype2/2.3.9/freetype-2.3.9.tar.gz
 try_download http://"$SF_MIRROR".dl.sourceforge.net/project/gs-fonts/gs-fonts/8.11%20%28base%2035%2C%20GPL%29/ghostscript-fonts-std-8.11.tar.gz
 try_download http://"$SF_MIRROR".dl.sourceforge.net/project/wvware/libwmf/0.2.8.4/libwmf-0.2.8.4.tar.gz
@@ -34,11 +37,10 @@ try_download http://www.ijg.org/files/jpegsrc.v7.tar.gz
 try_download http://dl.maptools.org/dl/libtiff/tiff-3.8.2.tar.gz
 try_download http://www.littlecms.com/lcms-1.18a.tar.gz
 try_download http://ghostscript.com/releases/ghostscript-8.70.tar.gz
-try_download ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.2.39.tar.bz2
+try_download ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.2.39.tar.gz
 try_download ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.5.4-10.tar.gz
 
 # Freetype.
-tar zxf freetype-2.3.9.tar.gz
 cd freetype-2.3.9
 ./configure --prefix=$CONFIGURE_PREFIX
 make
@@ -46,7 +48,6 @@ sudo make install
 cd ..
 
 # LibPNG.
-tar jxf libpng-1.2.39.tar.bz2
 cd libpng-1.2.39
 ./configure --prefix=$CONFIGURE_PREFIX
 make
@@ -54,7 +55,6 @@ sudo make install
 cd ..
 
 # JPEGsrc.
-tar zxf jpegsrc.v7.tar.gz
 cd jpeg-7
 ln -s `which glibtool` ./libtool
 export MACOSX_DEPLOYMENT_TARGET=10.6
@@ -64,7 +64,6 @@ sudo make install
 cd ..
 
 # LibTIFF.
-tar zxf tiff-3.8.2.tar.gz
 cd tiff-3.8.2
 ./configure --prefix=$CONFIGURE_PREFIX
 make
@@ -72,7 +71,6 @@ sudo make install
 cd ..
 
 # LibWMF.
-tar zxf libwmf-0.2.8.4.tar.gz
 cd libwmf-0.2.8.4
 make clean
 ./configure
@@ -81,7 +79,6 @@ sudo make install
 cd ..
 
 # LCMS.
-tar zxf lcms-1.18a.tar.gz
 cd lcms-1.18
 make clean
 ./configure
@@ -90,7 +87,6 @@ sudo make install
 cd ..
 
 # GhostScript.
-tar zxf ghostscript-8.70.tar.gz
 cd ghostscript-8.70
 ./configure --prefix=$CONFIGURE_PREFIX
 make
@@ -98,11 +94,9 @@ sudo make install
 cd ..
 
 # GhostScript Fonts.
-tar zxf ghostscript-fonts-std-8.11.tar.gz
 sudo mv fonts $CONFIGURE_PREFIX/share/ghostscript
 
 # ImageMagick.
-tar zxf ImageMagick-6.5.4-10.tar.gz
 cd ImageMagick-6.5.4-10
 export CPPFLAGS=-I$CONFIGURE_PREFIX/include
 export LDFLAGS=-L$CONFIGURE_PREFIX/lib
