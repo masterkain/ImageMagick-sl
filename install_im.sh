7741842f2d7013a44ed3b5c30b45b1d706b6030c
Julio Javier Cicchelli <javier@rock-n-code.com>
2010-03-28 19:47:46 +0800

    Updated some download links to their latest releases.

--- install_im.sh
+++ install_im.sh
@@ -56,15 +56,15 @@ function decompress_applications () {
 }
 
 # Before running anything try to download all requires files, saving time.
-try_download http://"$SF_MIRROR".dl.sourceforge.net/project/freetype/freetype2/2.3.11/freetype-2.3.11.tar.gz
+try_download http://"$SF_MIRROR".dl.sourceforge.net/project/freetype/freetype2/2.3.11/freetype-2.3.12.tar.gz
 try_download http://"$SF_MIRROR".dl.sourceforge.net/project/gs-fonts/gs-fonts/8.11%20%28base%2035%2C%20GPL%29/ghostscript-fonts-std-8.11.tar.gz
 try_download http://"$SF_MIRROR".dl.sourceforge.net/project/wvware/libwmf/0.2.8.4/libwmf-0.2.8.4.tar.gz
-try_download http://www.ijg.org/files/jpegsrc.v7.tar.gz
+try_download http://www.ijg.org/files/jpegsrc.v8a.tar.gz
 try_download ftp://ftp.remotesensing.org/pub/libtiff/tiff-3.9.2.tar.gz
 try_download http://www.littlecms.com/lcms-1.19.tar.gz
-try_download http://ghostscript.googlecode.com/files/ghostscript-8.70.tar.gz
-try_download ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.2.43.tar.gz
-try_download ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.5.8-10.tar.gz
+try_download http://ghostscript.googlecode.com/files/ghostscript-8.71.tar.gz
+try_download ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.4.1.tar.gz
+try_download ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.6.0.tar.gz
 
 # Decompress applications.
 decompress_applications
@@ -73,7 +73,7 @@ echo "Starting..."
 
 # LibPNG.
 # Official PNG reference library.
-cd libpng-1.2.43
+cd libpng-1.4.1
 ./configure --prefix=$CONFIGURE_PREFIX
 make
 sudo make install
@@ -81,7 +81,7 @@ cd ..
 
 # JPEG.
 # Library for JPEG image compression.
-cd jpeg-7
+cd jpeg-8a
 ln -s `which glibtool` ./libtool
 export MACOSX_DEPLOYMENT_TARGET=$DEPLOYMENT_TARGET
 ./configure --enable-shared --prefix=$CONFIGURE_PREFIX
@@ -100,7 +100,7 @@ cd ..
 
 # GhostScript.
 # Interpreter for the PostScript language and for PDF.
-cd ghostscript-8.70
+cd ghostscript-8.71
 ./configure --prefix=$CONFIGURE_PREFIX
 make
 sudo make install
@@ -113,7 +113,7 @@ sudo mv fonts $CONFIGURE_GS_FONT
 
 # The FreeType Project.
 # A free, high-quality and portable font engine.
-cd freetype-2.3.11
+cd freetype-2.3.12
 ./configure --prefix=$CONFIGURE_PREFIX
 make
 sudo make install
@@ -138,7 +138,7 @@ cd ..
 
 # ImageMagick.
 # Software suite to create, edit, and compose bitmap images.
-cd ImageMagick-6.5.8-10
+cd ImageMagick-6.6.10
 export CPPFLAGS=-I$CONFIGURE_PREFIX/include
 export LDFLAGS=-L$CONFIGURE_PREFIX/lib
 ./configure --prefix=$CONFIGURE_PREFIX $IMAGEMAGICK_ARGUMENTS --with-gs-font-dir=$CONFIGURE_GS_FONT
