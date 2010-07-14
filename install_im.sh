08357da50d19fc48d8a9c141746685bc7f3a5b34
Claudio Poli <masterkain@gmail.com>
2010-07-14 14:01:58 +0200

    close #6

--- install_im.sh
+++ install_im.sh
@@ -73,7 +73,7 @@ echo "Starting..."
 
 # LibPNG.
 # Official PNG reference library.
-cd libpng-1.4.2
+cd libpng-1.4.3
 ./configure --prefix=$CONFIGURE_PREFIX
 make
 sudo make install
@@ -138,7 +138,7 @@ cd ..
 
 # ImageMagick.
 # Software suite to create, edit, and compose bitmap images.
-cd ImageMagick-6.6.2-4
+cd ImageMagick-6.6.3-0
 export CPPFLAGS=-I$CONFIGURE_PREFIX/include
 export LDFLAGS=-L$CONFIGURE_PREFIX/lib
 ./configure --prefix=$CONFIGURE_PREFIX $IMAGEMAGICK_ARGUMENTS --with-gs-font-dir=$CONFIGURE_GS_FONT/fonts
