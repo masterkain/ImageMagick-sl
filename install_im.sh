22086bc69f2c63e540163fe66c153bc497381536
Michael Young <michaelyoung@Michael-Youngs-MacBook-Air.local>
2010-12-30 14:18:40 +0800

    updated libpng and ImageMagick downloads to work with latest available versions

--- install_im.sh
+++ install_im.sh
@@ -73,7 +73,7 @@ echo "Starting..."
 
 # LibPNG.
 # Official PNG reference library.
-cd libpng-1.4.4
+cd libpng-1.4.5
 ./configure --prefix=$CONFIGURE_PREFIX
 make
 sudo make install
@@ -138,7 +138,7 @@ cd ..
 
 # ImageMagick.
 # Software suite to create, edit, and compose bitmap images.
-cd ImageMagick-6.6.6-1
+cd ImageMagick-6.6.6-7
 export CPPFLAGS=-I$CONFIGURE_PREFIX/include
 export LDFLAGS=-L$CONFIGURE_PREFIX/lib
 ./configure --prefix=$CONFIGURE_PREFIX $IMAGEMAGICK_ARGUMENTS --with-gs-font-dir=$CONFIGURE_GS_FONT/fonts
