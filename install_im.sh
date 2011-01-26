1bc893db8c6d1acb1399f6233c2e4bc8759e19f7
Joon You <joon@Joon-Yous-Mac-Pro.local>
2011-01-25 22:50:53 -0500

    corrected the version of ImageMagick

--- install_im.sh
+++ install_im.sh
@@ -64,7 +64,7 @@ try_download http://download.osgeo.org/libtiff/tiff-3.9.4.tar.gz
 try_download http://"$SF_MIRROR".dl.sourceforge.net/project/lcms/lcms/2.0/lcms2-2.0a.tar.gz
 try_download http://ghostscript.googlecode.com/files/ghostscript-9.00.tar.gz
 try_download ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.4.5.tar.gz
-try_download ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.6.6-7.tar.gz
+try_download ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.6.6-10.tar.gz
 
 # Decompress applications.
 decompress_applications
@@ -138,7 +138,7 @@ cd ..
 
 # ImageMagick.
 # Software suite to create, edit, and compose bitmap images.
-cd ImageMagick-6.6.6-7
+cd ImageMagick-6.6.6-10
 export CPPFLAGS=-I$CONFIGURE_PREFIX/include
 export LDFLAGS=-L$CONFIGURE_PREFIX/lib
 ./configure --prefix=$CONFIGURE_PREFIX $IMAGEMAGICK_ARGUMENTS --with-gs-font-dir=$CONFIGURE_GS_FONT/fonts
